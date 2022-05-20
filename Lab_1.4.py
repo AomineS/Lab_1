159349dc4817554c0f3ae4433e3a66d80dded90f
AomineS <100805092+AomineS@users.noreply.github.com>
2022-05-21 01:38:21 +0300

    Add files via upload

---
+++ Lab_1.4.py
@@ -0,0 +1,12 @@
+x1 = int(input("Ввести x1: "))
+y1 = int(input("Ввести y1: "))
+x2 = int(input("Ввести x2: "))
+y2 = int(input("Ввести y2: "))
+AA = y2 - y1
+BB = x2 - x1
+P = 2*(AA+BB)
+S = AA * BB
+print('Периметр прямокутника: ', P)
+print('Площа прямокутника: ', S)
+
+
---
+++ Lab_1.5.py
@@ -0,0 +1,8 @@
+A = float(input("A= "))
+B = float(input("B= "))
+A != 0
+x = -B/A
+print('Значення x=', x)
+
+
+
